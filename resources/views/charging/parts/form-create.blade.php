9ff62c26ee30c385fedc8d4628ef3a6a0b820ac9
Marcus <m.jochimsen@demv.de>
2020-12-27 09:57:49 +0100

    new wording

--- resources/views/charging/parts/form-create.blade.php
+++ resources/views/charging/parts/form-create.blade.php
@@ -11,7 +11,7 @@
             </div>
             <input id="load-{{ $battery->id }}" type="number"
                    class="form-control @error('load') is-invalid @enderror" name="load"
-                   value="{{ old('load') }}" min="0" max="100" step="1" placeholder="aktueller Ladezustand" required autofocus>
+                   value="{{ old('load') }}" min="0" max="100" step="1" placeholder="neuer Ladezustand" required autofocus>
             <div class="input-group-append">
                 <span class="input-group-text">%</span>
                 <button type="submit" class="btn btn-primary" data-toggle="tooltip" data-placement="bottom" title="Speichern">
